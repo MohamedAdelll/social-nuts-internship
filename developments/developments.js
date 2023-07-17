f5d0e4e000634e74d226876a97289cb4bf2ef9e7
mohamed ashour <ashour521@gmail.com>
2023-07-17 15:25:51 +0300

    fixing responsiveness bugs

--- developments/developments.js
+++ developments/developments.js
@@ -1,1 +1,29 @@
 // import
+import Swiper from "swiper";
+
+const swiperOptions = {
+  // Optional parameters
+  //   direction: "vertical",
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: ".swiper-pagination",
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  // And if we need scrollbar
+  scrollbar: {
+    el: ".swiper-scrollbar",
+  },
+  autoplay: {
+    delay: 5000,
+  },
+};
+
+const swiper = new Swiper(".swiper", swiperOptions);
--- newsLetterModal/newLetterModal.js
+++ newsLetterModal/newLetterModal.js
@@ -4,6 +4,8 @@ const closeModalButton = document.querySelector("#closeModal");
 const video = document.querySelector("video");
 const mouseAnimator = document.querySelector(".mouse-animation");
 
+header.classList.remove("hide");
+
 closeModalButton.addEventListener("click", () => {
   newsLetter.classList.add("hide");
   document.body.classList.remove("inert");
