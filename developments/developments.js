2534309d36fe230c43faa5a23ce81395c74da767
mohamed ashour <ashour521@gmail.com>
2023-07-19 15:03:18 +0300

    mediaCenter section added

--- developments/developments.js
+++ developments/developments.js
@@ -1,13 +1,9 @@
-// import
-import { Swiper } from "swiper";
-import { Navigation, Autoplay, EffectFade } from "swiper";
-// import { Navigation, Pagination } from "swiper/modules";
 const descriptionAnimationTime = 1000;
 const totalDescriptionTime = 5000;
 
 const pageNumber = document.querySelector(".page-number");
 const swiperOptions = {
-  modules: [Navigation, Autoplay, EffectFade],
+  // modules: [Navigation, Autoplay, EffectFade],/
   slidesPerView: 1,
   // Optional parameters
   direction: "horizontal",
@@ -34,33 +30,24 @@ const firstDescription = document.querySelector(
   `.slide-description[data-index="0"]`
 );
 firstDescription.classList.add("translateY-0");
-let lastActiveIndex = 0;
+let lastActiveIndex = null;
 
 swiper.on("slideChange", (data) => {
-  const index = data.activeIndex % 3 === 0 ? 3 : data.activeIndex % 3;
+  const activeSlide = document.querySelector(".swiper-slide-active");
+  if (!activeSlide) return;
+  const index = +activeSlide.dataset.swiperSlideIndex;
   if (lastActiveIndex === index) return;
-  console.log({
-    index,
-  });
-  const newActiveDescriptions = document.querySelectorAll(
-    `.slide-description[data-index="${index - 1}"]`
-  );
-  newActiveDescriptions.forEach((newActiveDescription) =>
-    newActiveDescription?.classList.add("translateY-0")
-  );
 
-  setTimeout(
-    () => {
-      const currentActiveDescriptions = document.querySelectorAll(
-        `.slide-description[data-index="${index - 1}"]`
-      );
-      currentActiveDescriptions.forEach((currentActiveDescription) =>
-        currentActiveDescription?.classList.remove("translateY-0")
-      );
-    },
+  //   // const activeDescription = activeSlide.querySelector(".slide-description");
+  //   // activeDescription.classList.add("translateY-0");
 
-    4000
-  );
-  pageNumber.textContent = `0${index}`;
+  //   // setTimeout(
+  //   //   () => {
+  //   //     activeDescription.classList.remove("translateY-0");
+  //   //   },
+
+  //   //   4000
+  //   // );
+  pageNumber.textContent = `0${index + 1}`;
   lastActiveIndex = index;
 });
